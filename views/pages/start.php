25f9be2fdda888b6e817632cc31bafc4d277e613
Dave Arroyo <arroyo.daveil@gmail.com>
2017-08-29 19:49:59 +0800

    Update element placement

--- views/pages/start.php
+++ views/pages/start.php
@@ -1,4 +1,19 @@
-<div class="container">
+<style type="text/css">
+	.faux-table{
+		display:table;
+		height:70%;
+		width:100%;
+	}
+	.faux-row{
+		display:table-row;
+	}
+	.faux-col{
+		display:table-cell;
+		vertical-align: middle;
+		text-align:center;
+	}
+</style>
+<div class="container" style="padding-top:3rem;">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="progress">
@@ -8,35 +23,40 @@
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-1">
-			<button type="button" class="btn btn-default" >
-			   &nbsp;<span class="glyphicon glyphicon-chevron-left" ></span> &nbsp;
-			</button>
-		</div>
-		<div class="col-md-10">
-			<p id="question">
-				Which of these do you enjoy eating the most?
-			</p>
-			<div id="options" class="list-group">
-				<button type="button" class="list-group-item">
-					A Plate of Sushi
-				</button>
-				<button type="button" class="list-group-item">
-					A Fruit & Vegetable Salad Wrap
-				</button>
-				<button type="button" class="list-group-item">
-					A Bacon Cheeseburger
-				</button>
-				<button type="button" class="list-group-item">
-					A Bag of Cheetos
+	<div class="faux-table">
+		<div class="faux-row">
+			<div class="faux-col" style="padding-top:15rem;">
+				<button type="button" class="btn btn-default" >
+				   &nbsp;<span class="glyphicon glyphicon-chevron-left" ></span> &nbsp;
 				</button>
 			</div>
-		</div>
-		<div class="col-md-1">
-			<button type="button" class="btn btn-default" >
-			   &nbsp;<span class="glyphicon glyphicon-chevron-right" ></span> &nbsp;
-			</button>
+			<div class="faux-col">
+				<div id="qa-content">
+					<p id="question">
+						Which of these do you enjoy eating the most?
+					</p>
+					
+					<div id="options" class="list-group" style="margin-bottom:-100px;margin-top:80px;">
+						<button type="button" class="list-group-item">
+							A Plate of Sushi
+						</button>
+						<button type="button" class="list-group-item">
+							A Fruit & Vegetable Salad Wrap
+						</button>
+						<button type="button" class="list-group-item">
+							A Bacon Cheeseburger
+						</button>
+						<button type="button" class="list-group-item">
+							A Bag of Cheetos
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="faux-col" style="padding-top:15rem;">
+				<button type="button" class="btn btn-default" >
+				   &nbsp;<span class="glyphicon glyphicon-chevron-right" ></span> &nbsp;
+				</button>
+			</div>
 		</div>
 	</div>
 </div>
