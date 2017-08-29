ad99d8d73dc8fc2d7aa9425cb5ffce4391dc3d0e
Dave Arroyo <arroyo.daveil@gmail.com>
2017-08-29 19:33:09 +0800

    Base q&a page

---
+++ views/pages/start.php
@@ -0,0 +1,42 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="progress">
+			  <div class="progress-bar  progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">
+				<span class="sr-only">10% Complete</span>
+			  </div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-1">
+			<button type="button" class="btn btn-default" >
+			   &nbsp;<span class="glyphicon glyphicon-chevron-left" ></span> &nbsp;
+			</button>
+		</div>
+		<div class="col-md-10">
+			<p id="question">
+				Which of these do you enjoy eating the most?
+			</p>
+			<div id="options" class="list-group">
+				<button type="button" class="list-group-item">
+					A Plate of Sushi
+				</button>
+				<button type="button" class="list-group-item">
+					A Fruit & Vegetable Salad Wrap
+				</button>
+				<button type="button" class="list-group-item">
+					A Bacon Cheeseburger
+				</button>
+				<button type="button" class="list-group-item">
+					A Bag of Cheetos
+				</button>
+			</div>
+		</div>
+		<div class="col-md-1">
+			<button type="button" class="btn btn-default" >
+			   &nbsp;<span class="glyphicon glyphicon-chevron-right" ></span> &nbsp;
+			</button>
+		</div>
+	</div>
+</div>
