514a925a6ec4d2f2b833337bdd4c3559cceed6b0
Dave Arroyo <arroyo.daveil@gmail.com>
2017-08-29 19:22:33 +0800

    Basic intro page

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,6 +1,25 @@
 <div class="container">
-	<h2>What's your food traveller's personality?</h2>
-	<p>Lorem ipsum</p>
-	
-	<a href="start" class="btn btn-default">START THE QUIZ</a>
+	<h2 class="text-center">What's your food traveller's personality?</h2>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<p>
+				Are you an adventurous or conservative foodie? 
+				Do you travel for the sights or the tastes?<br/>
+				Knowing what kind of food traveller you are helps you plan trips that become memorable and meaningful for you.
+			</p>
+			<img src="img/intro.jpg" alt="Traveller's Plate Intro" />
+		</div>
+	</div>
+		
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<p>
+				Find out with this quiz that gives you<br/>
+				in-depth details about your preferences
+			</p>
+			<br />
+			<a href="start" class="btn btn-default btn-lg">START THE QUIZ</a>
+			
+		</div>
+	</div>
 </div>
