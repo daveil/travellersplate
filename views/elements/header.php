ba1101937aa208f1183c436a63cf99d1a8317502
Dave Arroyo <arroyo.daveil@gmail.com>
2017-08-29 19:12:30 +0800

    Update template files and home

--- views/elements/header.php
+++ views/elements/header.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
-		<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
 		<div class="container-fluid">
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,6 @@
-Homepage
+<div class="container">
+	<h2>What's your food traveller's personality?</h2>
+	<p>Lorem ipsum</p>
+	
+	<a href="start" class="btn btn-default">START THE QUIZ</a>
+</div>
